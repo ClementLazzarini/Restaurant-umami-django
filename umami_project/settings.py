c63effa0eeff28769b96c7ffaf221ae222339ee6
Clement Lazzarini <clement.lazzarini71@gmail.com>
2025-10-10 09:56:07 +0200

    fix: Configuration de ALLOWED_HOSTS pour la production

--- umami_project/settings.py
+++ umami_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zemk*v_vsufhk_4#sjhk^8&qo2ien0t1m91cpz#js1!a7g6amu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['clemlazztech.pythonanywhere.com', '127.0.0.1']
 
 
 # Application definition
